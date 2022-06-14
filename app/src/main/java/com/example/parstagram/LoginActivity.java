16f6b5d75b9e1967479fbfbf147127f7b12f9338
Shaniya Clement <shaniyaclement@fb.com>
2022-06-14 00:03:02 -0700

    Added Logout button, and programmed to logout user and return to LoginActivity on click.

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,13 +2,74 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private Button btnLogin;
+    private EditText etUsername;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //checks to see if there is already a user logged in
+        // if user is logged in, skip log in & go to MainActivity
+        if(ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        btnLogin = findViewById(R.id.btnLogin);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.i(TAG, "onClick Login Button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                //check credentials
+                loginUser(username, password);
+            }});
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempt to Login user: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                // if info doesn't match records, send error message
+                if (e != null) {
+                    Log.e(TAG, "Login Issue " + e );
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT);
+                    return;
+                }
+                //else, go to main activity and send success message
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
+            }
+        });
+
+    }
+
+    private void goMainActivity() {
+        //navigates to mainActivity and prevents from going back to login with back button
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); //returns null
+                goLoginActivity();
+            }
+        });
 
         ParseObject firstObject = new  ParseObject("FirstClass");
         firstObject.put("message","Hey ! First message from android. Parse is now connected");
@@ -23,5 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MainActivity","Object saved.");
             }
         });
+    }
+
+    private void goLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
